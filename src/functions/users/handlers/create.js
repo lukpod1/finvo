59425450212b53069a55f4983cf1b4eff43a314a
Jose Lucas <lukasalvespod1@gmail.com>
2021-04-21 20:31:39 -0300

    WIP: add function create user

---
+++ src/functions/users/handlers/create.js
@@ -0,0 +1,29 @@
+import { v4 as uuid } from 'uuid';
+import middleware from '../../../libs/middleware';
+import validator  from '@middy/validator';
+import dynamoDb from '../../../libs/dynamodb';
+import { success, failure } from '../../../libs/response';
+import createError from 'http-errors';
+
+const createUser = async (event, context) => {
+
+    const { username } = event.body;
+
+    const user = {
+        id: uuid(),
+        username
+    };
+
+    try {
+        await dynamoDb.put({
+            TableName: 'dev-Users',
+            Item: user
+        });
+        success(user);
+    } catch (error) {
+        failure(user);
+        throw new createError.InternalServerError(error);
+    }
+}
+
+export const handler = middleware(createUser);
---
+++ src/libs/dynamodb.js
@@ -0,0 +1,21 @@
+import { DynamoDB } from "aws-sdk";
+
+let options = {};
+
+if (process.env.IS_OFFLINE) {
+    options = {
+        region: 'localhost',
+        endpoint: 'http://localhost:4566'
+    }
+}
+
+const dynamodb = new DynamoDB.DocumentClient(options);
+
+export default {
+    put: (params) => dynamodb.put(params).promise(),
+    get: (params) => dynamodb.get(params).promise,
+    query: (params) => dynamodb.query(params).promise(),
+    scan: (params) => dynamodb.scan(params).promise(),
+    update: (params) => dynamodb.update(params).promise(),
+    delete: (params) => dynamodb.delete(params).promise(),
+}
