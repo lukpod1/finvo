9642f7ae6b79df3352d76ac90fb33e749011310b
José Lucas Alves da Silva <lukasalvespod1@gmail.com>
2021-04-29 02:36:39 -0300

    feat(WIP): added lambda create account

---
+++ src/functions/accounts/handlers/create.js
@@ -0,0 +1,31 @@
+import { v4 as uuid } from 'uuid';
+import middleware from '../../../libs/middleware';
+import dynamoDb from '../../../libs/dynamodb';
+import { success, failure } from '../../../libs/response';
+import createError from 'http-errors';
+// import { validateField } from '../../../libs/validateField';
+
+const createAccount = async (event, context) => {
+
+    const { name, balance, userId } = event.body;
+
+    const account = {
+        id: uuid(),
+        userId,
+        name,
+        balance
+    };
+
+    try {
+        await dynamoDb.put({
+            TableName: process.env.ACCOUNTS_TABLE,
+            Item: account
+        });
+        return success(account);
+    } catch (error) {
+        failure(account);
+        throw new createError.InternalServerError(error);
+    }
+};
+
+export const handler = middleware(createAccount);
