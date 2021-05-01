07442cae209b04b3d8aa3f94dba82c3d95a43b94
José Lucas <lukasalvespod1@gmail.com>
2021-04-30 22:00:15 -0300

    feat: added lambda remove account

--- src/functions/accounts/handlers/remove.js
+++ src/functions/accounts/handlers/remove.js
@@ -1,6 +1,6 @@
 import middleware from '../../../libs/middleware';
 import dynamoDb from '../../../libs/dynamodb';
-import { success } from '../../../libs/response';
+import { success, failure } from '../../../libs/response';
 import createError from 'http-errors';
 // import { validateField } from '../../../libs/validateField';
 
@@ -9,20 +9,19 @@ const removeAccount = async (event, context) => {
     const { id } = event.pathParameters;
     const { userId } = event.body;
 
-    console.log(path);
-
     try {
         const response = await dynamoDb.delete({
             TableName: process.env.ACCOUNTS_TABLE,
             Key: {id, userId}
         });
 
-        if (!response) {
+        if (!response.Attributes) {
             throw new createError.NotFound(`Account with ID "${id}" not found`);
         }
 
         return success(response.Attributes);
     } catch (error) {
+        failure(error);
         throw new createError.InternalServerError(error);
     } 
 };
