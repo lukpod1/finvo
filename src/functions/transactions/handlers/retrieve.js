f61b13dd8f91601441d6d2e9c00d50af98013c3c
José Lucas Alves da Silva <lukasalvespod1@gmail.com>
2021-06-02 11:58:21 -0300

    added retrieve trasaction function

---
+++ src/functions/transactions/handlers/retrieve.js
@@ -0,0 +1,23 @@
+import dynamoDb from '../../../libs/dynamodb';
+import createError from 'http-errors';
+import middleware from '../../../libs/middleware';
+import { success } from '../../../libs/response';
+
+const getTransactionById = async (event) => {
+
+    const { id, accountId } = event.pathParameters;
+
+    try {
+        const response = await dynamoDb.get({
+            TableName: process.env.TRANSACTIONS_TABLE,
+            Key: { id, accountId }
+        });
+
+        return success(response.Item);
+    } catch (error) {
+        console.error(error);
+        throw new createError.InternalServerError(error);
+    }
+};
+
+export const handler = middleware(getTransactionById);
