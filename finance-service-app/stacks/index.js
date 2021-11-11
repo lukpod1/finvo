c37fcd8696fec9b26b5b048145398e5d5f1e8f91
José Lucas <lukasalvespod1@gmail.com>
2021-11-10 22:21:55 -0300

    adding api basicusing SST

---
+++ finance-service-app/stacks/index.js
@@ -0,0 +1,16 @@
+import StorageStack from "./StorageStack";
+import ApiStack from "./ApiStack";
+
+export default function main(app) {
+  // Set default runtime for all functions
+  app.setDefaultFunctionProps({
+    runtime: "nodejs12.x"
+  });
+
+  // Add more stacks
+  const storageStack = new StorageStack(app, "storage");
+  
+  new ApiStack(app, "api", {
+    table: storageStack.table,
+  });
+}
