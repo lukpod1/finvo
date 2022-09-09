493d49c8170a5fea0d86cc709368690b010c451b
José Lucas <lukasalvespod1@gmail.com>
2022-09-09 03:30:13 -0300

    fix: removing unused code

--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -1,10 +1,7 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import Header from "../components/Header";
-import Navbar from "../components/Navbar";
-import { AUTH_USER_TOKEN_KEY } from "../utils/constants";
+
 
 export default function DashBoard() {
   return (
@@ -16,19 +13,7 @@ export default function DashBoard() {
           <div className="px-4 py-6 sm:px-0">
             <div className="border-4 border-dashed border-gray-200 rounded-lg h-96">
               <h1>Usuario Logado</h1>
-              <p>{localStorage.getItem(AUTH_USER_TOKEN_KEY)}</p>
-              {/* {checkUserAuth ? (
-                <>
-                  <h1>Usuario Logado</h1>
-                  <p>{localStorage.getItem(AUTH_USER_TOKEN_KEY)}</p>
-                </>
-              ) : (
-                <>
-                <h1>Não Autorizado</h1>
-                <p>Token: {localStorage.getItem(AUTH_USER_TOKEN_KEY)}</p>
-                <b>Validação: {checkUserAuth}</b>
-                </>
-              )} */}
+              <p></p>
             </div>
           </div>
         </div>
