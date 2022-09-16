2cc284f2056b5887702fc0ca7f486e93ef34de9a
José Lucas <lukasalvespod1@gmail.com>
2022-09-16 01:28:38 -0300

    feat(WIP): mock cards dashboard

--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -14,11 +14,20 @@ export default function DashBoard() {
       <main>
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="px-4 py-6 sm:px-0">
-
-            <p>{user.fullName}</p>
-            <p>{user.email}</p>
-
-
+            <div className="flex flex-row justify-center py-10">
+              <div className="flex flex-col rounded-xl bg-white w-80 h-28">
+                <div className="h-full m-4 font-medium text-slate-400">Current Balance</div>
+                <div className="h-full m-4 font-medium text-2xl">R$ 20.000,00</div>
+              </div>
+              <div className="flex flex-col rounded-xl bg-white mx-10 w-80 h-28">
+                <div className="h-full m-4 font-medium text-slate-400">Expenses</div>
+                <div className="h-full m-4 font-medium text-2xl">R$ 20.000,00</div>
+              </div>
+              <div className="flex flex-col rounded-xl bg-white w-80 h-28">
+                <div className="h-full m-4 font-medium text-slate-400">Invoices</div>
+                <div className="h-full m-4 font-medium text-2xl">R$ 20.000,00</div>
+              </div>
+            </div>
           </div>
         </div>
       </main>
