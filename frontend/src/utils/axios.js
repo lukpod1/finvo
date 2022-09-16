e12ceb6cfcf0680b322da2a58e599cf1afd5d839
José Lucas <lukasalvespod1@gmail.com>
2022-09-16 01:27:26 -0300

    fix(WIP): correcting authorization parameter format

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 import { AUTH_USER_TOKEN_KEY } from '../utils/constants'
 
-const instance = axios.create()
-instance.defaults.headers.common["Authorization"] = localStorage.getItem(AUTH_USER_TOKEN_KEY)
+const instance = axios.create({
+  headers: {
+    "Authorization": `Bearer ${localStorage.getItem(AUTH_USER_TOKEN_KEY)}`
+  }
+})
 
 export default instance;
