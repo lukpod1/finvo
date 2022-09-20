a9c7e7c5a99d153ce2ee75f4ed32caea5a451b11
José Lucas <lukasalvespod1@gmail.com>
2022-09-19 23:40:33 -0300

    feat: added constant endpoint accounts

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,3 +1,4 @@
 export const AUTH_USER_TOKEN_KEY = 'TOKEN';
 
 export const ENDPOINT_USERS = `${import.meta.env.VITE_USERS_API_URL}/users`;
+export const ENDPOINT_ACCOUNTS = `${import.meta.env.VITE_ACCOUNTS_API_URL}/accounts`;
