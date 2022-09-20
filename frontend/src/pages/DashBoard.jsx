f6c1150a23f4eaf2a00bf1c61986cd8dbdf4d75d
José Lucas <lukasalvespod1@gmail.com>
2022-09-19 23:41:09 -0300

    feat(WIP): added call to get all accounts of user

--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -1,11 +1,27 @@
+import axios from "axios";
 import { useState, useEffect, useContext } from "react";
 
 import Header from "../components/Header";
 import { Context } from "../components/Navbar";
 
+import { AUTH_USER_TOKEN_KEY, ENDPOINT_ACCOUNTS } from '../utils/constants'
 
 export default function DashBoard() {
+  const [count, setCount] = useState(0);
   const user = useContext(Context)
+
+  useEffect(() => {
+    setTimeout(() => {
+      const token = localStorage.getItem(AUTH_USER_TOKEN_KEY)
+      axios.get(`${ENDPOINT_ACCOUNTS}?userId=${user.id}`, {
+        headers: { "Authorization": `Bearer ${token}` }
+      }).then((response) => {
+        if (response.status === 200) {
+          setCount(response.data.count);
+        }
+      }, 1000)
+    })
+  }, [user])
 
   return (
     <>
@@ -16,20 +32,21 @@ export default function DashBoard() {
           <div className="px-4 py-6 sm:px-0">
             <div className="flex flex-row justify-center py-10">
               <div className="flex flex-col rounded-xl bg-white w-80 h-28">
-                <div className="h-full m-4 font-medium text-slate-400">Current Balance</div>
-                <div className="h-full m-4 font-medium text-2xl">R$ 20.000,00</div>
+                <div className="h-4 m-4 font-medium text-slate-400">Current Balance</div>
+                <div className="h-4 m-3 font-medium text-4xl">R$ 20.000,00</div>
               </div>
               <div className="flex flex-col rounded-xl bg-white mx-10 w-80 h-28">
-                <div className="h-full m-4 font-medium text-slate-400">Expenses</div>
-                <div className="h-full m-4 font-medium text-2xl">R$ 20.000,00</div>
+                <div className="h-4 m-4 font-medium text-slate-400">Expenses</div>
+                <div className="h-4 m-3 font-medium text-4xl">R$ 20.000,00</div>
               </div>
               <div className="flex flex-col rounded-xl bg-white w-80 h-28">
-                <div className="h-full m-4 font-medium text-slate-400">Invoices</div>
-                <div className="h-full m-4 font-medium text-2xl">R$ 20.000,00</div>
+                <div className="h-4 m-4 font-medium text-slate-400">Invoices</div>
+                <div className="h-4 m-3 font-medium text-4xl">R$ 20.000,00</div>
               </div>
             </div>
           </div>
         </div>
+        Accounts: {count}
       </main>
     </>
   )
