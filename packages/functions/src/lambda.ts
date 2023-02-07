8918017a74e220e0f7de0436730b650577e2745a
José Lucas <lukasalvespod1@gmail.com>
2023-02-07 13:59:56 -0300

    updated sst for v2

--- packages/functions/src/lambda.ts
+++ packages/functions/src/lambda.ts
@@ -1,5 +1,5 @@
 import { ApiHandler } from "sst/node/api";
-import { Time } from "finance-service/core/time";
+import { Time } from "@finance-service/core/time";
 
 export const handler = ApiHandler(async (_evt) => {
   return {
--- sst.config.ts
+++ sst.config.ts
@@ -1,18 +1,18 @@
 import { SSTConfig } from "sst";
-import { API } from "./stacks/MyStack"
+import { API } from "./stacks/MyStack";
+import { Api } from "sst/constructs";
 
 export default {
   config(_input) {
     return {
       name: "finance-service",
-      region: "us-east-1"
-    }
+      region: "us-east-1",
+    };
   },
   stacks(app) {
     app.setDefaultFunctionProps({
-      runtime: "nodejs16.x",
-      architecture: "arm_64",
+      runtime: "nodejs14.x",
     })
     app.stack(API)
-  }
-} satisfies SSTConfig
+  },
+} satisfies SSTConfig;
