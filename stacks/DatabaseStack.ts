ee5e5a383bc5453d2fb4640822f4cc801d227d44
José Lucas <lukasalvespod1@gmail.com>
2023-04-07 20:22:56 -0300

    added tables accounts and transactions

--- stacks/DatabaseStack.ts
+++ stacks/DatabaseStack.ts
@@ -3,12 +3,37 @@ import { StackContext, Table } from "sst/constructs";
 export function Database({ stack }: StackContext) {
   const dbUsers = new Table(stack, "users", {
     fields: {
-        id: "string",
+      id: "string",
+      name: "string",
+      email: "string",
+      picture: "string"
     },
     primaryIndex: { partitionKey: "id" },
   });
 
+  const dbAccounts = new Table(stack, "accounts", {
+    fields: {
+      id: "string",
+      userId: "string",
+      name: "string",
+      balance: "number",
+    },
+    primaryIndex: { partitionKey: "id" , sortKey: "userId"},
+  });
+
+  const dbTransactions = new Table(stack, "transactions", {
+    fields: {
+      id: "string",
+      amount: "number",
+      date: "string",
+      type: "string",
+      accountId: "string",
+      userId: "string",
+    },
+    primaryIndex: { partitionKey: "id", sortKey: "accountId" },
+  });
+
   return {
-    dbUsers,
+    dbUsers, dbAccounts, dbTransactions
   }
 }
