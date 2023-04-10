be22f82426bcc30d6e9cf68674c2269ec574f492
José Lucas <lukasalvespod1@gmail.com>
2023-04-10 03:49:36 -0300

    removed put item account in auth handler

--- packages/functions/src/auth.ts
+++ packages/functions/src/auth.ts
@@ -3,7 +3,6 @@ import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
 import { Table } from "sst/node/table"
 import { marshall } from "@aws-sdk/util-dynamodb"
 import { Config } from "sst/node/config";
-import { randomUUID } from "crypto";
 
 declare module "sst/node/auth" {
     export interface SessionTypes {
@@ -23,7 +22,7 @@ export const handler = AuthHandler({
                 const user = tokenset.claims();
 
                 const ddb = new DynamoDBClient({});
-                const newUser = await ddb.send(new PutItemCommand({
+                await ddb.send(new PutItemCommand({
                     TableName: Table.users.tableName,
                     Item: marshall({
                         id: user.sub,
@@ -32,19 +31,6 @@ export const handler = AuthHandler({
                         name: user.given_name,
                     })
                 }))
-
-                const account = await ddb.send(new PutItemCommand({
-                    TableName: Table.accounts.tableName,
-                    Item: marshall({
-                        id: randomUUID(),
-                        userId: user.sub,
-                        name: "Default Account",
-                        balance: 0,
-                    })
-                }))
-
-                console.log("NEW USER:", newUser);
-                console.log("NEW ACCOUNT:", account);
 
                 return Session.parameter({
                     redirect: `${process.env.SITE_URL}/login`|| "http://localhost:3000/login",
--- stacks/AuthStack.ts
+++ stacks/AuthStack.ts
@@ -1,11 +1,9 @@
-import { Api, Auth, Config, StackContext, use } from "sst/constructs";
+import { Auth, Config, StackContext, use } from "sst/constructs";
 import { FrontEnd } from "./FrontEndStack";
-import { Database } from "./DatabaseStack";
 import { Session } from "./SessionStack";
 
 export function Authentication({ stack }: StackContext) {
 
-    const { dbAccounts } = use(Database);
     const sessionStack = use(Session);
     const frontEndStack = use(FrontEnd);
 
@@ -14,7 +12,7 @@ export function Authentication({ stack }: StackContext) {
     const auth = new Auth(stack, "auth", {
         authenticator: {
             handler: "packages/functions/src/auth.handler",
-            bind: [sessionStack.sessionApi, GOOGLE_CLIENT_ID, dbAccounts],
+            bind: [sessionStack.sessionApi, GOOGLE_CLIENT_ID],
             environment: {
                 SITE_URL: frontEndStack.site.url || "http://localhost:3000",
             },
