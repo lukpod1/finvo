595eeaacd7bf71c70ef2274e592b91d349b2c3f6
José Lucas <lukasalvespod1@gmail.com>
2023-05-18 06:26:31 -0300

    [WIP] update table transaction

--- stacks/AccountStack.ts
+++ stacks/AccountStack.ts
@@ -2,12 +2,12 @@ import { Api, StackContext, use } from "sst/constructs";
 import { Database } from "./DatabaseStack";
 
 export function Accounts({ stack }: StackContext) {
-    const { dbAccounts, dbTransactions } = use(Database)
+    const { dbAccounts } = use(Database)
 
     const accountsApi = new Api(stack, "accountsApi", {
         defaults: {
             function: {
-                bind: [dbAccounts, dbTransactions]
+                bind: [dbAccounts ]
             }
         },
         routes: {
