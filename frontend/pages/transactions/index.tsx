754f3af08ade0ddde97995e20250eb06e2caccea
José Lucas <lukasalvespod1@gmail.com>
2023-05-19 03:36:20 -0300

    fix: refresh datas of transactions

--- frontend/pages/transactions/index.tsx
+++ frontend/pages/transactions/index.tsx
@@ -39,7 +39,7 @@ export default function Transactions(props: any) {
             return { ...transaction, accountName };
         });
         setTransactionsData(transactionsWithAccountNames);
-    }, []);
+    }, [accounts, transactions]);
     
     return (
         <Layout>
