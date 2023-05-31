28fb9faae7b9eaec1e49b17c024d808ad4bc91a3
José Lucas <lukasalvespod1@gmail.com>
2023-05-31 12:26:32 -0300

    feat: apply again datadog lib

--- sst.config.ts
+++ sst.config.ts
@@ -5,7 +5,7 @@ import { FrontEnd } from "./stacks/FrontEndStack";
 import { Accounts } from "./stacks/AccountStack";
 import { Session } from "./stacks/SessionStack";
 import { Transaction } from "./stacks/TransactionStack";
-// import { Datadog } from "datadog-cdk-constructs-v2";
+import { Datadog } from "datadog-cdk-constructs-v2";
 import { Stack } from "sst/constructs";
 
 export default {
@@ -15,7 +15,7 @@ export default {
       region: "us-east-1",
     };
   },
-  stacks(app) {
+  async stacks(app) {
     app.stack(Database)
     app.stack(Session)
     app.stack(Accounts)
@@ -23,22 +23,20 @@ export default {
     app.stack(FrontEnd)
     app.stack(Authentication)
 
-    // if (!app.local) {
-    //   await app.finish();
+    if (!app.local) {
+      await app.finish();
 
-    //   app.node.children.forEach((stack) => {
-    //     if (stack instanceof Stack) {
-    //       const datadog = new Datadog(stack, "Datadog", {
-    //         nodeLayerVersion: 91,
-    //         extensionLayerVersion: 43,
-    //         apiKey: process.env.DATADOG_API_KEY,
-    //       });
-
-    //       datadog.addLambdaFunctions(stack.getAllFunctions());
-    //     }
-
-    //   })
-    // }
+      app.node.children.forEach((stack) => {
+        if (stack instanceof Stack) {
+          const datadog = new Datadog(stack, "Datadog", {
+            nodeLayerVersion: 91,
+            extensionLayerVersion: 43,
+            apiKey: process.env.DATADOG_API_KEY,
+          });
+          datadog.addLambdaFunctions(stack.getAllFunctions());
+        }
+      })
+    }
 
   },
 } satisfies SSTConfig;
