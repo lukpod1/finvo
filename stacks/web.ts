b67349441e8ca4581bab151442df9ea499c411f2
Jsosé Lucas <lukasalvespod1@gmail.com>
2023-12-06 11:02:48 -0300

    feat: start vue

--- stacks/web.ts
+++ stacks/web.ts
@@ -1,21 +1,28 @@
-import { NextjsSite, StackContext, use } from "sst/constructs";
+import { NextjsSite, StackContext, StaticSite, use } from "sst/constructs";
 import { API } from "./api";
 
 export function Web({ stack }: StackContext) {
 
 	const api = use(API);
 
-	const site = new NextjsSite(stack, "site", {
+	// const site = new NextjsSite(stack, "site", {
+	// 	customDomain:
+	// 		stack.stage === "prod" ? "finvo.net" : `${stack.stage}.finvo.net`,
+	// 	path: "packages/frontend",
+	// 	environment: {
+	// 		NEXT_PUBLIC_API_URL: api.url,
+	// 	},
+	// 	buildCommand: "npx open-next@0.7.0 build",
+	// });
+
+	const site = new StaticSite(stack, "vue", {
+		path: "packages/web/finvo",
 		customDomain:
 			stack.stage === "prod" ? "finvo.net" : `${stack.stage}.finvo.net`,
-		path: "packages/frontend",
 		environment: {
-			NEXT_PUBLIC_API_URL: api.url,
-		},
-		buildCommand: "npx open-next@0.7.0 build",
+			VITE_APP_API_URL: api.url
+		}
 	});
-
-	site.attachPermissions([api]);
 
 	api.setCors({
 		allowCredentials: true,
