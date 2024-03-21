0f499f863fad7fc450cfefe7a921ffb4eb29b5a0
Jsosé Lucas <lukasalvespod1@gmail.com>
2024-03-21 01:59:32 -0300

    feat: remove condition to set the custom domain

--- stacks/web.ts
+++ stacks/web.ts
@@ -1,13 +1,16 @@
-import { NextjsSite, StackContext, StaticSite, use } from "sst/constructs";
+import { NextjsSite, StackContext, use } from "sst/constructs";
 import { API } from "./api";
+import { DNS } from "./dns";
 
 export function Web({ stack }: StackContext) {
-
+	const dns = use(DNS);
 	const api = use(API);
 
 	const site = new NextjsSite(stack, "site", {
-		customDomain:
-		stack.stage === "prod" ? "finvo.net" : `${stack.stage}.finvo.net`,
+		customDomain: {
+			domainName: dns.domain,
+			hostedZone: dns.zone?.zoneName
+		},
 	 	path: "packages/frontend",
 	 	environment: {
 	 		NEXT_PUBLIC_API_URL: api.url,
