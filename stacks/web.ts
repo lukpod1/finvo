c07973bf05b04d8c4507a3e15a908356897c6321
Jsosé Lucas <lukasalvespod1@gmail.com>
2024-03-22 01:02:15 -0300

    fix: revert changes in custom domain

--- stacks/web.ts
+++ stacks/web.ts
@@ -3,14 +3,11 @@ import { API } from "./api";
 import { DNS } from "./dns";
 
 export function Web({ stack }: StackContext) {
-	const dns = use(DNS);
 	const api = use(API);
 
 	const site = new NextjsSite(stack, "site", {
-		customDomain: {
-			domainName: dns.domain,
-			hostedZone: dns.zone?.zoneName
-		},
+		customDomain:
+		stack.stage === "prod" ? "finvo.net" : `${stack.stage}.finvo.net`,
 	 	path: "packages/frontend",
 	 	environment: {
 	 		NEXT_PUBLIC_API_URL: api.url,
